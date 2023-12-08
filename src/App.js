e04f6905dde385e90d4d3c6ff5d2a19ec86635c3
shivansh srivastava <shivanshengineergkp@gmail.com>
2023-12-09 02:41:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Card from './components/card/card';
 
 function App() {
   return (
 <>
 <Navbar />
+<Card />
 </>
   );
 }
---
+++ src/components/card/card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./card.css";
+
+const card = () => {
+  return (
+    <div className="card_box">
+      <h2>ihbhhjj</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad,
+        necessitatibus! Labore eligendi magni eius fuga dicta. Suscipit,
+        molestiae quos? Aliquam consequatur nulla eveniet temporibus non ipsam
+        amet laborum accusantium adipisci!
+      </p>
+    </div>
+  );
+};
+
+export default card;
